19ed5c2f40e443aa02baf7be478b865a3047325b
Emanuell Paredes <emanuellp@hotmail.com>
2020-05-05 14:16:33 -0500

    add bootstrap, login html

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div>
+      <router-link to="/">Home</router-link> <br />
+      <router-link to="/login">Login</router-link>
     <router-view/>
   </div>
 </template>
